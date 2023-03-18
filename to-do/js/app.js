edbf708c3dc3754cf185f98b521f4f58ba04183c
Sanket Avinash Potghan <97453159+SanketSP01@users.noreply.github.com>
2023-03-18 17:30:57 +0530

    Add files via upload

---
+++ to-do/js/app.js
@@ -0,0 +1,35 @@
+const item = document.querySelector("#item");
+const toDoBox = document.querySelector("#to-do-box");
+
+item.addEventListener(
+    "keyup",
+    function(event){
+       if (event.key == "Enter") {
+        addToDO(this.value)
+        this.value = ""
+       }
+    }
+)
+
+const addToDO = (item) => {
+    const listitem = document.createElement("li");
+    listitem.innerHTML = `
+       ${item} 
+       <i class="fas fa-times"></i>
+    `;
+    listitem.addEventListener(
+        "click",
+        function () {
+            this.classList.toggle("done");
+        }
+    )
+    listitem.querySelector("i").addEventListener(
+        "click",
+        function () {
+            listitem.remove();
+        }
+        
+    )
+    toDoBox.appendChild(listitem)
+
+}
